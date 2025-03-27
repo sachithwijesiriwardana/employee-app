606033ba8e12a0112f9f6e0141f998e98a60511a
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-03-27 20:36:10 +0530

    Add routing and components for login, layout, dashboard, employee, and project pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { Component } from '@angular/core';
+import { LayoutComponent } from './pages/layout/layout.component';
+import path from 'path';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      {
+        path: 'employee',
+        component: EmployeeComponent,
+      },
+    ],
+  },
+];
